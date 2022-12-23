afd17d5dce7805a4c6bc0ced5c577b281e89569b
Petrovich <welcomeplaygame@gmail.com>
2022-12-23 16:58:47 +0200

    Innit

---
+++ src/main/java/ua/vadym/scanner/ScannerApplication.java
@@ -0,0 +1,20 @@
+package ua.vadym.scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ScannerApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScannerApplication.class, args);
+	}
+
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+}
